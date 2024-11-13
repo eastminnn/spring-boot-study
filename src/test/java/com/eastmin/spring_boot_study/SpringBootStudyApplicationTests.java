a40ad5e8099e14443e0a8c3958e2cf98ac3a5e8e
eastminnn <oh1334000@gmail.com>
2024-11-13 16:36:40 +0900

    add file

---
+++ src/test/java/com/eastmin/spring_boot_study/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastmin.spring_boot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
